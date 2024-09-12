e96808190b3e9e4094d3516ee2ea4aa5c401881d
ManuelCLopes <manuel_clopes@outlook.com>
2024-09-12 16:25:54 +0100

    Change host name

--- finance_dashboard/settings.py
+++ finance_dashboard/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True' 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'financetracker.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'finance-tracker.herokuapp.com']
 
 # Application definition
 
