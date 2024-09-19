0903720750d37324895105e997c2f91d051f796d
ManuelCLopes <manuel_clopes@outlook.com>
2024-09-19 01:04:01 +0100

    configure domain

--- finance_dashboard/settings.py
+++ finance_dashboard/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True' 
 
-ALLOWED_HOSTS = ['finance-tracker-5c42401d1c26.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['http://www.financetracker.pt','finance-tracker-5c42401d1c26.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
